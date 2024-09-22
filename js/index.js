00d66f0653aa3f32a661d825d0aaefe366b2632a
imamhossenbu <163906693+imamhossenbu@users.noreply.github.com>
2024-09-23 03:12:23 +0600

    add navbar sticky

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 function myBlog(){
     window.location.href='./blog.html';
-    // console.log('hello');
 }
+
+
+const nav = document.querySelector('header');
+window.addEventListener("scroll",function(){
+  if(document.documentElement.scrollTop > 0){
+    nav.classList.add("sticky");
+  }
+  else{
+    nav.classList.remove("sticky");
+  }
+})
